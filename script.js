841f8d14b90dc551b0052c8f4e95b16b532bdf6c
djmgit <djmdeveloper060796@gmail.com>
2018-02-16 17:23:59 +0530

    Added methods to read circuit file and create a graph

---
+++ script.js
@@ -0,0 +1,79 @@
+console.log("hello world");
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  console.log("success!!");
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
+function readSingleFile(evt) {
+  //Retrieve the first (and only!) File from the FileList object
+  var f = evt.target.files[0]; 
+
+  if (f) {
+    var r = new FileReader();
+    r.onload = function(e) { 
+      var contents = e.target.result;
+      processFile(contents);
+    }
+    r.readAsText(f);
+  } else { 
+    alert("Failed to load file");
+  }
+}
+
+function processFile(fileContent) {
+  var blocks = fileContent.split("\n\n");
+  
+  var declarations = blocks[0];
+  var inputs = blocks[1];
+  var outputs = blocks[2];
+  var operations = blocks.slice(3);
+
+  inputs = inputs.split("\n");
+  outputs = outputs.split("\n");
+  declarations = declarations.split("\n");
+
+  inputs = inputs.map(input => input.substring(input.indexOf('(') + 1, input.indexOf(')')));
+
+  outputs = outputs.map(output => output.substring(output.indexOf('(') + 1, output.indexOf(')')));
+
+  var operations_total = [];
+  for (var i = 0; i < operations.length; i++) {
+    var operationTemp = operations[i];
+    if (operationTemp !== undefined) {
+      operations_total = operations_total.concat(operationTemp.split("\n"));
+    }
+  }
+
+  var fileName = declarations[0].split(" ")[1];
+  var inputsNumber = declarations[1].split(" ")[1];
+  var outputsNumber = declarations[2].split(" ")[1];
+
+  var graph = {};
+
+  for (var i = 0; i < operations_total.length; i++) {
+    var opr = operations_total[i];
+    if (opr.length === 0) {
+      continue;
+    }
+    out = opr.split("=")[0].trim();
+    inp = opr.substring(opr.indexOf("(") + 1, opr.indexOf(")"));
+    inp = inp.split(", ");
+    for (var j = 0; j < inp.length; j++) {
+      var input = inp[j].trim();
+      if (graph[input] === undefined) {
+        graph[input] = [];
+      }
+      graph[input].push(out);
+    }
+  }
+
+  console.log(fileName);
+  console.log(inputsNumber);
+  console.log(outputsNumber);
+
+  console.log(graph);
+}
+
+document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
