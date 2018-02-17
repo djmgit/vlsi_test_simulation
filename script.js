167a14e72c5b14c51237df422314d344267c98c5
djmgit <djmdeveloper060796@gmail.com>
2018-02-17 14:27:17 +0530

    Completed topological sorting method

--- script.js
+++ script.js
@@ -22,13 +22,31 @@ function readSingleFile(evt) {
   }
 }
 
+function andGate(inputs) {
+  var output = inputs[0];
+  for (var i = 1; i < inputs.length; i++) {
+    output = output & inputs[i];
+  }
+
+  return output;
+}
+
+function orGate(inputs) {
+  var output = inputs[0];
+  for (var i = 1; i < inputs.length; i++) {
+    output = output | inputs[i];
+  }
+
+  return output;
+}
+
 function topologicalSort(graph) {
   var indergree = {}
   var nodes = [];
   var keys = Object.keys(graph);
 
   // get all nodes
-  for (var i = 0; i < keyes.length; i++) {
+  for (var i = 0; i < keys.length; i++) {
     nodes.push(keys[i]);
     nodes = nodes.concat(graph[keys[i]]);
   }
@@ -40,15 +58,42 @@ function topologicalSort(graph) {
   for (var i = 0; i < keys.length; i++) {
     var dependencies = graph[keys[i]];
     for (j = 0; j < dependencies.length; j++) {
-      if (!indergree[dependencies[j]]) {
-        indergree[dependencies[j]] = 0;
-      }
       indergree[dependencies[j]] += 1;
     }
   }
 
   sorted = [];
   queue = [];
+  for (var i = 0; i < nodes.length; i++) {
+    if (indergree[nodes[i]] === 0) {
+      queue.push(nodes[i]);
+    }
+  }
+
+  while (queue.length !== 0) {
+    var node = queue.shift();
+    sorted.push(node);
+    var dependencies = graph[node];
+    if (dependencies) {
+      for (var i = 0; i < dependencies.length; i++) {
+        indergree[dependencies[i]] -= 1;
+        if (indergree[dependencies[i]] === 0) {
+          queue.push(dependencies[i]);
+        }
+      }
+    }
+  }
+
+  return sorted;
+}
+
+function simulate(logicCircuit, sorted, inputVector) {
+  var output = inputVector;
+
+  for (var i = 0; i < sorted.length; i++) {
+    continue;
+  }
+
 }
 
 function processFile(fileContent) {
@@ -80,15 +125,23 @@ function processFile(fileContent) {
   var outputsNumber = declarations[2].split(" ")[1];
 
   var graph = {};
+  var logicCircuit = {};
 
   for (var i = 0; i < operations_total.length; i++) {
     var opr = operations_total[i];
     if (opr.length === 0) {
       continue;
     }
-    out = opr.split("=")[0].trim();
-    inp = opr.substring(opr.indexOf("(") + 1, opr.indexOf(")"));
-    inp = inp.split(", ");
+    var out = opr.split("=")[0].trim();
+    var inp = opr.substring(opr.indexOf("(") + 1, opr.indexOf(")"));
+    var inp = inp.split(", ");
+    var gate = opr.split("=")[1].trim().substring(0, opr.indexOf("("));
+
+    logicCircuit[out] = {
+      'operation': gate,
+      'inputs': inp
+    }
+
     for (var j = 0; j < inp.length; j++) {
       var input = inp[j].trim();
       if (graph[input] === undefined) {
@@ -129,7 +182,23 @@ function processFile(fileContent) {
 
   $('#my_canvas').springy({ graph: vizGraph });
 
+  var sortedGraph = topologicalSort(graph);
+  console.log("Topologically sorted graph");
+  console.log(sortedGraph);
 
+  // sample input
+
+  var inputVector = {
+    "G0": 1,
+    "G1": 0,
+    "G2": 1,
+    "G3": 0,
+    "G5": 1,
+    "G6": 0,
+    "G7": 1,
+  }
+
+  var output = simulate(logicCircuit, sortedGraph, inputVector);
 }
 
 document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
